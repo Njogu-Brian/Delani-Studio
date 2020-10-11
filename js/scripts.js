7250c802082fc7e67282b1620fb7523a7104dada
Njogu-brian <briannjogu85@gmail.com>
2020-10-11 20:40:18 +0300

    add js function

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+    $(".jumbotron#click").click(function() {
+      $(".show").toggle();
+      $(".hide").toggle();
+    });
+    $(".jumbotron#click1").click(function() {
+      $(".show1").toggle();
+      $(".hide1").toggle();
+    });
+    $(".jumbotron#click2").click(function() {
+      $(".show2").toggle();
+      $(".hide2").toggle();
+    });
+  });
+
+onclick(){
+alert("message recived "+name)
+}
